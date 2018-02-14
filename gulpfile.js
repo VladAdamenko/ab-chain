4e21989f01f431bb3d0f7df1ea9f521d6954ab1a
VladAdamenko <adamenko.vlv@gmail.com>
2018-02-14 16:32:04 +0200

    Change structure

--- gulpfile.js
+++ gulpfile.js
@@ -20,26 +20,26 @@ var gulp           = require('gulp'),
 gulp.task('browser-sync', function() {
 	browserSync({
 		server: {
-			baseDir: 'app'
+			baseDir: 'app/icoLanding'
 		},
 		notify: false
 	});
 });
 
 gulp.task('sass', ['headersass'], function() {
-	return gulp.src('app/sass/**/*.sass')
+	return gulp.src('app/icoLanding/sass/**/*.sass')
 		.pipe(sass({
 			includePaths: bourbon.includePaths
 		}).on("error", notify.onError()))
 		.pipe(rename({suffix: '.min', prefix : ''}))
 		.pipe(autoprefixer(['last 15 versions']))
 		.pipe(cleanCSS())
-		.pipe(gulp.dest('app/css'))
+		.pipe(gulp.dest('app/icoLanding/css'))
 		.pipe(browserSync.reload({stream: true}))
 });
 
 gulp.task('headersass', function() {
-	return gulp.src('app/header.sass')
+	return gulp.src('app/icoLanding/header.sass')
 		.pipe(sass({
 			includePaths: bourbon.includePaths
 		}).on("error", notify.onError()))
@@ -52,25 +52,25 @@ gulp.task('headersass', function() {
 
 gulp.task('libs', function() {
 	return gulp.src([
-		'app/libs/jquery/dist/jquery.min.js',
-		'app/libs/fancybox/jquery.fancybox.js',
-		'app/libs/owl.carousel/owl.carousel.min.js',
-		'app/libs/scroolly/jquery.scroolly.min.js'
+		'app/icoLanding/libs/jquery/dist/jquery.min.js',
+		'app/icoLanding/libs/fancybox/jquery.fancybox.js',
+		'app/icoLanding/libs/owl.carousel/owl.carousel.min.js',
+		'app/icoLanding/libs/scroolly/jquery.scroolly.min.js'
 		])
 		.pipe(concat('libs.min.js'))
 		.pipe(uglify())
-		.pipe(gulp.dest('app/js'));
+		.pipe(gulp.dest('app/icoLanding/js'));
 });
 
 gulp.task('watch', ['sass', 'libs', 'browser-sync'], function() {
-	gulp.watch('app/header.sass', ['headersass']);
-	gulp.watch('app/sass/**/*.sass', ['sass']);
-	gulp.watch('app/*.html', browserSync.reload);
-	gulp.watch('app/js/**/*.js', browserSync.reload);
+	gulp.watch('app/icoLanding/header.sass', ['headersass']);
+	gulp.watch('app/icoLanding/sass/**/*.sass', ['sass']);
+	gulp.watch('app/icoLanding/*.html', browserSync.reload);
+	gulp.watch('app/icoLanding/js/**/*.js', browserSync.reload);
 });
 
 gulp.task('imagemin', function() {
-	return gulp.src('app/img/**/*')
+	return gulp.src('app/icoLanding/img/**/*')
 		.pipe(cache(imagemin({
 			interlaced: true,
 			progressive: true,
@@ -81,7 +81,7 @@ gulp.task('imagemin', function() {
 });
 
 gulp.task('buildhtml', function() {
-  gulp.src(['app/*.html'])
+  gulp.src(['app/icoLanding/*.html'])
     .pipe(fileinclude({
       prefix: '@@'
     }))
@@ -94,17 +94,17 @@ gulp.task('removedist', function() { return del.sync('dist'); });
 gulp.task('build', ['removedist', 'buildhtml', 'imagemin', 'sass', 'libs'], function() {
 
 	var buildCss = gulp.src([
-		'app/css/fonts.min.css',
-		'app/css/main.min.css'
+		'app/icoLanding/css/fonts.min.css',
+		'app/icoLanding/css/main.min.css'
 		]).pipe(gulp.dest('dist/css'));
 
 	var buildFiles = gulp.src([
-		'app/.htaccess'
+		'app/icoLanding/.htaccess'
 	]).pipe(gulp.dest('dist'));
 
-	var buildFonts = gulp.src('app/fonts/**/*').pipe(gulp.dest('dist/fonts'));
+	var buildFonts = gulp.src('app/icoLanding/fonts/**/*').pipe(gulp.dest('dist/fonts'));
 
-	var buildJs = gulp.src('app/js/**/*').pipe(gulp.dest('dist/js'));
+	var buildJs = gulp.src('app/icoLanding/js/**/*').pipe(gulp.dest('dist/js'));
 
 });
 
